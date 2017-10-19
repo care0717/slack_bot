72a8a3e0178957881a714dce3811fffa70091259
asai <syousa.care0717@gmail.com>
2017-10-19 23:22:33 +0900

    update: schdulesクラスに

--- homepage_request.rb
+++ homepage_request.rb
@@ -18,44 +18,47 @@ class Schedules < Array
   include ArrayToSelfConvert
   def today
     res = select { |a_sche| a_sche[:day] == Date.today.strftime('%m-%d') }
-    res.each { |a_sche| a_sche.delete(:day) }
     return res
   end
 
   def latest_semi
-    [select { |a_sche| a_sche[:content].include?('ゼミ') }[-1]]
+    Schedules.new([select { |a_sche| a_sche[:content].include?('ゼミ') }[-1]])
   end
-end
 
-def to_text(schedules)
-  if schedules.empty?
-    return '今日の予定はありません．'
-  elsif schedules.size == 1
-    return strip_single_sch(schedules).values.join(' ')
-  else
-    res = []
-    schedules.each do |sche|
-      res.push(sche.values.join(' '))
+  def to_text
+    if self.empty?
+      return '予定はありません．'
+    elsif self.size == 1
+      return strip_single_sch(self).values.join(' ')
+    else
+      res = []
+      self.each do |sche|
+        res.push(sche.values.join(' '))
+      end
+      return res.join('¥n')
     end
-    return res.join('¥n')
+  end
+
+  def strip_single_sch(array)
+    return array[0] if array.class <= Array && array.size == 1
+    return array
   end
 end
 
-def strip_single_sch(array)
-  return array[0] if array.class <= Array && array.size == 1
-  return array
-end
+
+
+
 
 def html_to_text(schedule_html, text)
   case text
   when '今日' then
     schedule = to_schedules(schedule_html.css('.schedule').search('tr')[1..-1])
-    return to_text(schedule.today)
+    return schedule.today.to_text
   when 'mura' then
     schedule = to_schedules(schedule_html.css('.trip_sche').search('tr')[1..-1])
-    return to_text(schedule.today)
+    return schedule.today.to_text
   when 'ゼミ' then
     schedule = to_schedules(schedule_html.css('.schedule').search('tr')[1..-1])
-    return to_text(schedule.latest_semi)
+    return schedule.latest_semi.to_text
   end
 end
